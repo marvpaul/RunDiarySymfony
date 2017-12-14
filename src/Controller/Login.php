40675f4ea8338f98abea6a2fef4eb0a9d4c70ba1
Marvin Krüger <marvin.krueger@yahoo.de>
2017-12-14 17:08:48 +0100

    Redirected to profile after successfull login

--- src/Controller/Login.php
+++ src/Controller/Login.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 use App\Entity\User;
@@ -14,7 +15,7 @@ class Login extends Controller {
     /**
      * @Route("/login", name="login")
      */
-    public function login(Request $request, AuthenticationUtils $authUtils, AuthorizationCheckerInterface $authChecker){
+    public function login(Request $request, AuthenticationUtils $authUtils){
 
         // get the login error if there is one
         $error = $authUtils->getLastAuthenticationError();
@@ -26,5 +27,16 @@ class Login extends Controller {
             'last_username' => $lastUsername,
             'error'         => $error,
         ));
+    }
+
+    /**
+     * @Route("/after_login", name="after_login")
+     */
+    public function after_login(UserInterface $loggedin_user = null){
+        if($loggedin_user == null){
+            return $this->redirect('/public/');
+        } else{
+            return $this->redirect('/public/profile/'. $loggedin_user->getName());
+        }
     }
 }
