bf18cd21637e2ce5737d0b43079e390c8a2272d1
marvpaul <marvin.krueger@yahoo.de>
2019-03-01 13:54:42 +0100

    Disabled sessions for testing and changed validation strategies regarding newly created entries

--- src/Repository/EntryRepository.php
+++ src/Repository/EntryRepository.php
@@ -12,17 +12,4 @@ class EntryRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Entry::class);
     }
-
-    /*
-    public function findBySomething($value)
-    {
-        return $this->createQueryBuilder('e')
-            ->where('e.something = :value')->setParameter('value', $value)
-            ->orderBy('e.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
 }
